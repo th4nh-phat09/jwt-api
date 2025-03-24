9d428ff37b653627b6e2a3525bbad3761d16ebbb
th4nh-phat09 <thanphat354@gmail.com>
2025-03-24 20:12:23 +0700

    api login create access and refresh token

--- src/providers/JwtProvider.js
+++ src/providers/JwtProvider.js
@@ -1,1 +1,17 @@
-//
+import JWT from 'jsonwebtoken'
+const generateToken = async (payload, secretSignature, tokenLife) => {
+  try {
+    return JWT.sign(payload, secretSignature, { algorithm: 'HS256', expiresIn: tokenLife })
+  } catch (error) { throw new Error(error)}
+}
+
+const verifyToken = async (token, secretSignature) => {
+  try {
+    return JWT.verify(token, secretSignature)
+  } catch (error) { throw new Error(error)}
+}
+
+export const JwtProvider = {
+  generateToken,
+  verifyToken
+}
